b0d7ff3e65e32eef12eb6b6f166a688cd3a7896e
eliudkaruga <eliudkaruga97@gmail.com>
2024-05-26 16:14:59 +0300

    REST API with postgresQL database

---
+++ REST/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+
+app.listen(PORT, () => {
+    console.log(`app running at ${PORT}`)
+})
